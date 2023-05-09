fd701d39cba8e5d86642351d21df1cb2b3b76004
Rosemary Benny <rosemarybenny2000@gmail.com>
2023-05-09 22:28:49 +0530

    hardhat project created

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    }
+  }
+};
+
